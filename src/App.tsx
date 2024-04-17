daedcab9fa91d2385aabaebfa266fe90c7218b28
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-17 15:19:59 +0100

    search term filter working/ showing

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import { FormEvent, useState } from "react";
-import "./App.css";
+import "./App.scss";
 import EmployeeTiles from "./components/Employees/EmployeeTiles";
 import team from "./data/team";
 import SearchBox from "./components/SearchBox/SearchBox";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const [searchTerm , setSearchTerm] = useState<string>("")
-
-  const handleSearch = (event : FormEvent<HTMLInputElement>) => {
+  const handleSearch = (event: FormEvent<HTMLInputElement>) => {
     const cleanedInput = event.currentTarget.value.toLowerCase();
     setSearchTerm(cleanedInput);
-    return team.map((employee) => employee.name.toLowerCase().includes(searchTerm))
-  }
-
+  };
+  const filteredEmployees = team.filter((employee) =>
+    employee.name.toLowerCase().includes(searchTerm)
+  );
 
   return (
     <div className="main-page">
       <h1 className="main-page__title">Ticket Tracker</h1>
-      <SearchBox placeholder="Search by name" searchTerm={searchTerm} handleInput={handleSearch}/>
+      <SearchBox
+        placeholder="Search by name"
+        searchTerm={searchTerm}
+        handleInput={handleSearch}
+      />
       <div className="employee-container">
         {/* // need to map over team data to get each employee stuff */}
-        {team.map((employee) => (
+        {filteredEmployees.map((employee) => (
           <EmployeeTiles
             name={employee.name}
             id={employee.id}
